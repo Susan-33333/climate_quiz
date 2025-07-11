eb57a2f19291861203629bfcfc5aaddcc74079a0
qaz7000810 <qaz7000810@gmail.com>
2025-07-11 14:53:47 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/climate-quiz/', 
+  base: '/climate_quiz/', // <<< 跟你的 GitHub repo 名稱完全一致
 });
