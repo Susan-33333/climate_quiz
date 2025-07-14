6c688355431935db1c303cd997cd1787402ae973
qaz7000810 <qaz7000810@gmail.com>
2025-07-14 11:47:26 +0800

    ICON

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-const { defineConfig } = require('vite');
-const react = require('@vitejs/plugin-react');
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-module.exports = defineConfig({
-  base: '/climate_quiz/',
+export default defineConfig({
+  base: '/climate_quiz/', // ✅ GitHub Pages 子目錄對應
   plugins: [react()],
 });
