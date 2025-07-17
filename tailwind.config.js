fdc6cd8e82f4f71924746bb7f075c58e680dad4b
Susan-33333 <susanthree0719@gmail.com>
2025-07-17 11:45:53 +0800

    33

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {}
+    extend: {colors: {
+      quiz: {
+        500: '#f3bbe9ff',
+      },
+    },
   },
+},
   plugins: []
 };
